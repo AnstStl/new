b8b6774b4959af85c5c1eba61b5e051a36c51873
AnstStl <nastya.stolyarova2004@yandex.ru>
2024-05-29 15:56:52 +0700

    new commit

---
+++ 123.py
@@ -0,0 +1,17 @@
+f= 5*(216**3031) + 4*(36**3042) - 3*(6**3053) - 3064
+s =''
+cum=0
+while f>0:
+    s =s + str(f%6)git add –A
+    f = f//6
+print(sum(map(int, s)))
+print('2=', s.count('2'))
+print('3=', s.count('3'))
+print('4=', s.count('4'))
+print('5=', s.count('5'))
+print('0=', s.count('0'))
+n=2*2 + 3*3 + 4 + 5*6080
+print('n',n)
+# for i in range (1, len(s)+1):
+#     cum += i
+# print(cum)
